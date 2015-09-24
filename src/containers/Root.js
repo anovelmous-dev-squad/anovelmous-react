1665a3664832decde317f6ff789d4fd441ffd3be
Greg Ziegan <greg.ziegan@gmail.com>
2015-09-24 13:39:03 -0400

    remove all koa server code, client js only for now

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -1,8 +1,6 @@
 import React        from 'react';
 import { Router }   from 'react-router';
-import invariant    from 'invariant';
 import routes       from '../routes';
-import { RoutingContext } from 'react-router';
 import ReactRouterRelay from 'react-router-relay';
 
 export default class Root extends React.Component {
@@ -11,8 +9,7 @@ export default class Root extends React.Component {
   // history to use (memory, hash, browser). routingContext, on the other hand,
   // is provided by the server and provides a full router state.
   static propTypes = {
-    routerHistory  : React.PropTypes.object,
-    routingContext : React.PropTypes.object
+    routerHistory  : React.PropTypes.object
   }
 
   constructor () {
@@ -20,23 +17,14 @@ export default class Root extends React.Component {
   }
 
   renderRouter () {
-    invariant(
-      this.props.routingContext || this.props.routerHistory,
-      '<Root /> needs either a routingContext or routerHistory to render.'
-    );
-
-    if (this.props.routingContext) {
-      return <RoutingContext {...this.props.routingContext} />;
-    } else {
-      return (
-        <Router
-          history={this.props.routerHistory}
-          createElement={ReactRouterRelay.createElement}
+    return (
+      <Router
+        history={this.props.routerHistory}
+        createElement={ReactRouterRelay.createElement}
         >
-          {routes}
-        </Router>
-      );
-    }
+        {routes}
+      </Router>
+    );
   }
 
   render () {
