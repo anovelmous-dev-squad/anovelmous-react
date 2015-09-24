3d6ae6052f6490abd9076d6708cf29a88118d418
Greg Ziegan <greg.ziegan@gmail.com>
2015-09-24 17:20:18 -0400

    playing with layout, need to break into layout component

---
+++ src/components/NavPanel.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import Relay from 'react-relay';
+import Radium from 'radium';
+
+const styles = {
+  base: {
+    background: 'rgb(152, 152, 152)',
+    border: 0,
+    borderRadius: 4,
+    color: 'rgb(40, 40, 40)',
+    padding: 10
+  }
+};
+
+@Radium
+class NavPanel extends React.Component {
+  static propTypes = {
+    contributor: React.PropTypes.object.isRequired
+  }
+
+  render() {
+    const { contributor } = this.props;
+
+    return (
+      <div style={styles.base}>
+        <ul>
+          {contributor.novels.edges.map(edge => (
+            <li>{edge.node.title}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Relay.createContainer(NavPanel, {
+  fragments: {
+    contributor: () => Relay.QL`
+      fragment on Contributor {
+        id
+        name
+        novels(last: 10) {
+          edges {
+            node {
+              id
+              title
+            }
+          }
+        }
+      }
+    `
+  }
+});
--- src/views/LiveView.jsx
+++ src/views/LiveView.jsx
@@ -2,7 +2,19 @@ import React, { PropTypes } from 'react';
 import Relay from 'react-relay';
 import VoteCaster from 'components/VoteCaster';
 import Novel from 'components/Novel';
+import NavPanel from 'components/NavPanel';
+import { Grid, Row, Cell } from 'react-inline-grid';
+import Radium from 'radium';
 
+const styles = {
+  base: {
+    background: 'rgb(204, 193, 155)',
+    border: 0,
+    borderRadius: 4
+  }
+};
+
+@Radium
 class LiveView extends React.Component {
     static propTypes = {
       contributor: PropTypes.object.isRequired
@@ -11,13 +23,27 @@ class LiveView extends React.Component {
     render () {
       const { contributor } = this.props;
       return (
-        <div>
-          <h1>Hello {contributor.name}!</h1>
-          {contributor.novels.edges.map(edge => (
-            <Novel key={edge.node.id} novel={edge.node}/>
-          ))}
-          <VoteCaster tokens={contributor.vocabulary}/>
-        </div>
+        <Grid>
+          <Row is="around tablet-start phone-start">
+            <Cell is="2 tablet-2 phone-1">
+              <NavPanel contributor={contributor}/>
+            </Cell>
+            <Cell is="4 tablet-4 phone-4">
+              <div style={styles.base}>
+                <h1>Hello {contributor.name}!</h1>
+                  {contributor.novels.edges.map(edge => (
+                    <Novel key={edge.node.id} novel={edge.node}/>
+                  ))}
+              <VoteCaster tokens={contributor.vocabulary}/>
+              </div>
+            </Cell>
+            <Cell is="2 tablet-2 phone-4">
+              <div style={{background: 'rgb(245, 243, 223)'}}>
+                <h2>sup</h2>
+              </div>
+            </Cell>
+          </Row>
+        </Grid>
       );
     }
 }
@@ -45,6 +71,7 @@ export default Relay.createContainer(LiveView, {
           }
           ${VoteCaster.getFragment('tokens')}
         }
+        ${NavPanel.getFragment('contributor')}
       }
     `
   }
