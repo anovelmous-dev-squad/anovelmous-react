23d309abba7c3810d0bf0c1584ce94d37fac7319
Greg Ziegan <greg.ziegan@gmail.com>
2015-11-27 23:33:05 -0500

    add custom color theme

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import routes from '../routes';
 import { RelayRouter } from 'react-router-relay';
+import ThemeManager from 'material-ui/lib/styles/theme-manager';
+import ThemeDecorator from 'material-ui/lib/styles/theme-decorator';
+import lightTheme from '../styles/lightTheme';
 
+@ThemeDecorator(ThemeManager.getMuiTheme(lightTheme))
 export default class Root extends React.Component {
 
   static propTypes = {
