115eac312e96b8b3d632fb17ba0a610195618384
Greg Ziegan <greg.ziegan@gmail.com>
2015-11-28 01:15:36 -0500

    added circular progress to loading component

--- src/components/LoadingLarge.jsx
+++ src/components/LoadingLarge.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { CircularProgress } from 'material-ui';
 
 export default () => (
   <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-    <h1>Loading ...</h1>
+    <CircularProgress mode="indeterminate" size={2} />
     <img style={{maxHeight: '100%', maxWidth: '100%'}} src="/assets/AnovelmousLogoXLarge.png" />
   </div>
 );
