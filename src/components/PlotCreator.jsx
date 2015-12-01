ec590633ef01256ed9b95595ba73ecdd3681df63
Greg Ziegan <greg.ziegan@gmail.com>
2015-12-01 12:53:04 -0500

    add character count to plot creator

--- src/components/PlotCreator.jsx
+++ src/components/PlotCreator.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { TextField, RaisedButton } from 'material-ui';
+import Colors from 'material-ui/lib/styles/colors';
 
 export default class PlotCreator extends React.Component {
   static propTypes = {
@@ -26,19 +27,32 @@ export default class PlotCreator extends React.Component {
     this._resetFormData();
   }
 
+  _getErrorStyle = () => {
+    const { summary } = this.state;
+    if (summary.length > 3000) {
+      return { color: Colors.red900 };
+    }
+
+    return { color: summary.length < 2000 ? Colors.green400 : Colors.amber500 };
+  }
+
   render() {
     const { summary } = this.state;
     return (
       <div>
         <form onSubmit={this._handleOnSubmit}>
           <TextField
+            type="text"
             hintText="Write a back-of-book plot summary that you think this novel should have!"
             floatingLabelText="Plot Summary Proposal"
+            errorStyle={this._getErrorStyle()}
+            errorText={`${summary.length} / 3000`}
             value={summary}
             onChange={this._handleSummaryChange}
             multiLine
             fullWidth
-            rows={10} />
+            rows={10}
+            />
           <RaisedButton type="submit" label="Submit" primary />
         </form>
       </div>
