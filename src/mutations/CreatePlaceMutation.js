2d88e382ca525bbc5c434fa84c92972b8507fec2
Greg Ziegan <greg.ziegan@gmail.com>
2015-11-30 21:44:05 -0500

    fixed up mutations for new schema

--- src/mutations/CreatePlaceMutation.js
+++ src/mutations/CreatePlaceMutation.js
@@ -1,58 +1,44 @@
 import Relay from 'react-relay';
 
 export default class CreatePlaceMutation extends Relay.Mutation {
-  static fragments = {
-    viewer: () => Relay.QL`
-      fragment on Contributor {
-        id
-      }
-    `
-  };
   getMutation() {
     return Relay.QL`mutation{createPlace}`;
   }
+
+  getVariables() {
+    const { name, description, novel, contributor } = this.props;
+    return {
+      name,
+      description,
+      novelId: novel.id,
+      contributorId: contributor.id
+    };
+  }
+
   getFatQuery() {
     return Relay.QL`
-      fragment on CreatePlacePayload {
-        placeEdge
-        viewer {
+      fragment on CreatePlace {
+        contributor {
           places
         }
       }
     `;
   }
+
   getConfigs() {
-    return [{
-      type: 'RANGE_ADD',
-      parentName: 'viewer',
-      parentID: this.props.viewer.id,
-      connectionName: 'places',
-      edgeName: 'placeEdge',
-      rangeBehaviors: {
-        '': 'append'
+    return [];
+  }
+
+  static fragments = {
+    novel: () => Relay.QL`
+      fragment on Novel {
+        id
       }
-    }];
-  }
-  getVariables() {
-    const { name, description, novelId } = this.props;
-    return {
-      name,
-      description,
-      novelId
-    };
-  }
-  getOptimisticResponse() {
-    const { name, description, viewer } = this.props;
-    return {
-      characterEdge: {
-        node: {
-          name,
-          description
-        }
-      },
-      viewer: {
-        id: viewer.id
+    `,
+    contributor: () => Relay.QL`
+      fragment on Contributor {
+        id
       }
-    };
-  }
+    `
+  };
 }
--- src/mutations/CreatePlotItemMutation.js
+++ src/mutations/CreatePlotItemMutation.js
@@ -1,58 +1,44 @@
 import Relay from 'react-relay';
 
 export default class CreatePlotItemMutation extends Relay.Mutation {
+  getMutation() {
+    return Relay.QL`mutation{createPlotItem}`;
+  }
+
+  getVariables() {
+    const { name, description, novel, contributor } = this.props;
+    return {
+      name,
+      description,
+      novelId: novel.id,
+      contributorId: contributor.id
+    };
+  }
+
+  getFatQuery() {
+    return Relay.QL`
+      fragment on CreatePlotItem {
+        contributor {
+          plotitems
+        }
+      }
+    `;
+  }
+
+  getConfigs() {
+    return [];
+  }
+
   static fragments = {
-    viewer: () => Relay.QL`
+    novel: () => Relay.QL`
+      fragment on Novel {
+        id
+      }
+    `,
+    contributor: () => Relay.QL`
       fragment on Contributor {
         id
       }
     `
   };
-  getMutation() {
-    return Relay.QL`mutation{createPlotItem}`;
-  }
-  getFatQuery() {
-    return Relay.QL`
-      fragment on CreatePlotItemPayload {
-        plotItemEdge
-        viewer {
-          plotItems
-        }
-      }
-    `;
-  }
-  getConfigs() {
-    return [{
-      type: 'RANGE_ADD',
-      parentName: 'viewer',
-      parentID: this.props.viewer.id,
-      connectionName: 'plotItems',
-      edgeName: 'plotItemEdge',
-      rangeBehaviors: {
-        '': 'append'
-      }
-    }];
-  }
-  getVariables() {
-    const { name, description, novelId } = this.props;
-    return {
-      name,
-      description,
-      novelId
-    };
-  }
-  getOptimisticResponse() {
-    const { name, description, viewer } = this.props;
-    return {
-      plotItemEdge: {
-        node: {
-          name,
-          description
-        }
-      },
-      viewer: {
-        id: viewer.id
-      }
-    };
-  }
 }
