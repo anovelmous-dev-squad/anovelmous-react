4975ad2398626ccd08bcc1ff3f204634df397461
Greg Ziegan <greg.ziegan@gmail.com>
2015-12-04 11:50:40 -0500

    colored voting arrows

--- src/components/ScoreCard.jsx
+++ src/components/ScoreCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Paper, IconButton, Card, CardHeader, CardText, CardActions } from 'material-ui';
+import Colors from 'material-ui/lib/styles/colors';
 
 export default class ScoreCard extends React.Component {
   static propTypes = {
@@ -7,37 +8,49 @@ export default class ScoreCard extends React.Component {
     score: React.PropTypes.number.isRequired,
     title: React.PropTypes.string.isRequired,
     description: React.PropTypes.string.isRequired,
-    onUpvote: React.PropTypes.func.isRequired
+    onUpvote: React.PropTypes.func,
+    onDownvote: React.PropTypes.func,
   };
+
+  static defaultProps = {
+    onUpvote: () => {},
+    onDownvote: () => {},
+  }
+
+  _handleUpvote = () => {
+    return this.props.onUpvote(this.props.id);
+  }
+
+  _handleDownvote = () => {
+    return this.props.onDownvote(this.props.id);
+  }
 
   render() {
     const { score, title, description } = this.props;
     return (
-
-        <Paper style={{padding: "12px", margin: "6px 0px"}}>
-
-          <table>
-            <tbody>
-
-            <tr style={{verticalAlign : "top"}}>
-              <td>
-                <IconButton iconClassName="material-icons"
-                            onClick={this._handleUpvote}>arrow_upward</IconButton>
-                <div style={{textAlign : "center"}}> {score} </div>
-                <IconButton iconClassName="material-icons">arrow_downward</IconButton>
-              </td>
-
-              <td>
-                <h2>{title}</h2>
-                {description}
-              </td>
-
-            </tr>
-            </tbody>
-          </table>
-
-        </Paper>
-
+      <Paper style={{padding: 12, margin: "6px 0px"}}>
+        <table>
+          <tbody>
+          <tr style={{verticalAlign : "top"}}>
+            <td>
+              <IconButton
+                iconStyle={{color: Colors.red900}}
+                iconClassName="material-icons"
+                onClick={this._handleUpvote}>arrow_upward</IconButton>
+              <div style={{textAlign : "center"}}> {score} </div>
+              <IconButton
+                iconStyle={{color: Colors.grey700}}
+                iconClassName="material-icons"
+                onClick={this._handleDownvote}>arrow_downward</IconButton>
+            </td>
+            <td>
+              <h2>{title}</h2>
+              {description}
+            </td>
+          </tr>
+          </tbody>
+        </table>
+      </Paper>
     );
   }
 }
