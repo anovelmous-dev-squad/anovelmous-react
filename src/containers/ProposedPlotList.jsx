8181068a99d9f45502dd8059272110b1f7dec6d8
Greg Ziegan <greg.ziegan@gmail.com>
2015-12-04 14:19:48 -0500

    update schema and add mutation for upvote/downvote

--- src/containers/ProposedPlotList.jsx
+++ src/containers/ProposedPlotList.jsx
@@ -2,12 +2,34 @@ import React from 'react';
 import Relay from 'react-relay';
 import ScoreCard from 'components/ScoreCard';
 
+import UpdateVoteScoreMutation from 'mutations/UpdateVoteScoreMutation';
 
 class ProposedPlotList extends React.Component {
   static propTypes = {
     contributor: React.PropTypes.object.isRequired,
     plots: React.PropTypes.object.isRequired,
   };
+
+  constructor(props) {
+    super(props);
+  }
+
+  _updateVoteScore(resourceId, addend) {
+    const { contributor } = this.props;
+    Relay.Store.update(
+      new UpdateVoteScoreMutation({
+        resourceId, contributor, addend
+      })
+    );
+  }
+
+  _handleUpvote = (plotId) => {
+    this._updateVoteScore(plotId, 1);
+  }
+
+  _handleDownvote = (plotId) => {
+    this._updateVoteScore(plotId, -1);
+  }
 
   renderPlotCard(plot) {
     const { contributor } = this.props;
@@ -33,6 +55,9 @@ class ProposedPlotList extends React.Component {
 }
 
 export default Relay.createContainer(ProposedPlotList, {
+  initialVariables: {
+    contributorId: '',
+  },
   fragments: {
     contributor: () => Relay.QL`
       fragment on Contributor {
